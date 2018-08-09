86c9ac912f2c29de7199af2587ef9021ad5ad192
gavrichenko <gavr.imba@mail.ru>
2018-08-09 11:44:56 +0300

    server logic

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-const express = require('express'),
-  defaultPort = 3000,
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const Task = require('./api/models/todoListModel'); //created model loading here
+const routes = require('./api/routes/todoListRoutes'); //importing route
+
+// setting for web-server
+const defaultPort = 3000,
   app = express(),
   port = process.env.PORT || defaultPort;
 
-app.listen(port);
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+app.listen(port);
 console.log('RESTful API server started on: ' + port);
